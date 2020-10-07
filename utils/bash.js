0e7a7345c83fbb6612010d7c54750da98c14342b
Praveen Ravichandran <me@pravinba9495.dev>
2020-10-06 23:38:28 -0400

    Update linter search space

--- utils/bash.js
+++ utils/bash.js
@@ -1,31 +1,36 @@
 const { execSync } = require("child_process");
-const { argv } = require('yargs');
+const { argv } = require("yargs");
 
 exports.ffprobe = (path) => {
-	const { streams } = JSON.parse(execSync(`ffprobe -show_streams -show_entries streams:format=filename -of json '${path}'`, {
-		stdio: 'pipe'
-	}).toString());
-	return {
-		streams
-	}
-}
+  const { streams } = JSON.parse(
+    execSync(
+      `ffprobe -show_streams -show_entries streams:format=filename -of json '${path}'`,
+      {
+        stdio: "pipe",
+      }
+    ).toString()
+  );
+  return {
+    streams,
+  };
+};
 
 exports.ffmpeg = (path, params, output) => {
-	const command = `ffmpeg -y -i '${path}' ${params} '${output}'`;
-	if (argv.v) {
-		console.log(`[EXECUTING] ${command}`);
-	}
-	return execSync(command, {
-		stdio: 'pipe'
-	}).toString();
-}
+  const command = `ffmpeg -y -i '${path}' ${params} '${output}'`;
+  if (argv.v) {
+    console.log(`[EXECUTING] ${command}`);
+  }
+  return execSync(command, {
+    stdio: "pipe",
+  }).toString();
+};
 
 exports.rename = (path, newPath) => {
-	const command = `mv '${path}' '${newPath}'`;
-	if (argv.v) {
-		console.log(`[EXECUTING] ${command}`);
-	}
-	return execSync(command, {
-		stdio: 'pipe'
-	}).toString();
-}
+  const command = `mv '${path}' '${newPath}'`;
+  if (argv.v) {
+    console.log(`[EXECUTING] ${command}`);
+  }
+  return execSync(command, {
+    stdio: "pipe",
+  }).toString();
+};
